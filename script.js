f39c3ecc4f226eba1c5cf199ce2ce57455b704f7
Alison lemes <alison.pereira@eaportal.org>
2024-06-02 19:44:34 +0000

    criação de helper responsavel  por mudar o tema

--- script.js
+++ script.js
@@ -1,52 +1,16 @@
-let tema = "claro"
-
-const botaotema=document.querySelector(".tema button ")
-botaotema.addEventListener("click",TrocarTema)
+import { TrocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 
+
+
+const botaoTema=document.querySelector(".tema button ")
 const body =document.querySelector("body")
 
 
-function TrocarTema(){
-    
-    if(localStorage.getItem ("tema")){
-        
-    tema=localStorage.getItem("tema")
+botaoTema.addEventListener("click",()=> {
+    TrocarTema(body,botaoTema)
+})
 
-    }
+verificarTema(body,botaoTema)
 
 
-    if(tema === "claro"){
-        body.classList.add("escuro")
-        localStorage.setItem("tema","escuro")
-     
-    } else{
-        body.classList.remove("escuro")
-        localStorage.setItem("tema","claro")
-    }
-
-
-
-}
-
-
-
-
-
-
-
-function verificarTema(){
-    if(localStorage.getItem("tema")) {
-        tema= localStorage.getItem("tema")
-    }
-
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-
-    }
-
-
-}
-
-verificarTema()
