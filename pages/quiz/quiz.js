f4dc09286e2b64ab5950d17ed7ef2c8c353504a4
Alison lemes <alison.pereira@eaportal.org>
2024-06-02 23:21:21 +0000

    adicionada a função de guardar a resposta e o id da resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,8 @@ const body =document.querySelector("body")
 let  quiz = {}
 let  pontos = 0
 let pergunta=1
+let resposta = ""
+let idInputResposta=""
 
 
 botaoTema.addEventListener("click",()=> {
@@ -83,7 +85,7 @@ function montarPergunta(){
         <section class="alternativas">
             <form action="">
             <label for="alternativa_a" >
-                <input type="radio"  id="alternativa_a"  name="alternativa">
+                <input type="radio"  id="alternativa_a"  name="alternativa" value=" ${alterarSinais(quiz.questions[pergunta-1].options[0])}">
 
                 <div>
                     <span>A</span>
@@ -95,7 +97,7 @@ function montarPergunta(){
 
 
             <label for="alternativa_b">
-                <input type="radio" id="alternativa_b" name="alternativa">
+                <input type="radio" id="alternativa_b" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[1])}">
 
                 <div>
                     <span>B</span>
@@ -105,7 +107,7 @@ function montarPergunta(){
             </label>
 
             <label for="alternativa_c">
-                <input type="radio" id="alternativa_c" name="alternativa">
+                <input type="radio" id="alternativa_c" name="alternativa" value=" ${alterarSinais(quiz.questions[pergunta-1].options[2])}">
 
 
                 <div>
@@ -115,7 +117,7 @@ function montarPergunta(){
             </label>
 
             <label for="alternativa_d">
-                <input type="radio" id="alternativa_d" name="alternativa">
+                <input type="radio" id="alternativa_d" name="alternativa" value=" ${alterarSinais(quiz.questions[pergunta-1].options[3])}" >
 
                 <div>
                     <span>D</span>
@@ -140,11 +142,26 @@ function alterarSinais(texto){
 }
 
 
+function guardarResposta(evento){
+    resposta=evento.target.value
+    idInputResposta=evento.target.id
+
+ 
+}
+
+
 
 async function iniciar(){
     alterarAssunto()
    await buscarPerguntas()
     montarPergunta()
+
+
+    const inputsResposta=document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click",guardarResposta)
+    })
+
 
 }
 
